f3d88987d77c01fba0a49b332d08fb074e0ccf75
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-04-23 14:10:39 +0100

    parted out some blocks of code into seperate functions, checks for duplicate theme id and theme path prevented

--- my_scripts/item_scraper.py
+++ my_scripts/item_scraper.py
@@ -3,13 +3,16 @@ from selenium.webdriver.common.by import By
 from database import DatabaseManagment
 import time, requests, os
 
+import responses
+
+RESP = responses.Response()
+DB = DatabaseManagment()
 
 class Scraper():
 
     def __init__(self) -> None:
         self.driver = webdriver.Chrome()
         self.url = "https://www.bricklink.com/catalogTree.asp?itemType=S"
-        self.db = DatabaseManagment()
 
 
     def setup(self) -> None:
@@ -38,7 +41,7 @@ class Scraper():
         page_counter = 1
         row_counter = 2
 
-        for i in range(num_items):
+        for _ in range(num_items):
             #click next page if 50th item on page is parsed
             if row_counter % 50 == 0 and num_items != 50:
                 page_counter += 1
@@ -61,20 +64,58 @@ class Scraper():
                 item_id = self.driver.find_element(By.XPATH, f'//*[@id="ItemEditForm"]/table[1]/tbody/tr/td/table/tbody/tr[{row_counter}]/td[2]/font/a[1]').text
                 print(item_id)
                 theme_details[item_type].append(item_id)
-            except:pass
+            except:
+                pass
             #move onto the next item on the page
             row_counter += 1
 
         return theme_details
+    
+
+    def remove_brackets_from_theme_name(self, themes):
+        #remove (num) of items in theme from theme name. Some themes have '(', ')' in their name e.g. '(other)'
+        #loop backwards of each theme:str and find the first '(' char. Split the string at that position
+        for theme_index, theme in enumerate(themes):
+            for char_index, char in enumerate(theme[::-1].strip("     ")):
+                if char == "(":
+                    theme_name = theme[:len(theme)-char_index-2]
+                    #replace theme names with name without brackets
+                    themes[theme_index] = theme_name
+                    break
+
+    def split_theme_name_by_first_char(self, theme):
+        #find first char in theme name to split.
+        for char in theme:
+            if char != " ":
+                return char
+            
+
+    def update_details_dict(self, details:list, num_spaces:int, index:int, parents:list, theme:str):
+        #store a list of dicts storing "theme" and "num_spaces" to compare current theme with previous theme(s)
+        details.append({"theme":theme.strip("     ") + "~", "num_spaces":num_spaces})
+
+        #if a new level of indentaion is reached add the current theme as a new sub-parent theme
+        if details[index]["num_spaces"] > details[index-1]["num_spaces"]:
+            parents.append(details[index-1]["theme"])
+
+        #if the previous themes spaces is < than current themes spaces the remove last element from parents as it is not a parent to any sub themes
+        elif details[index]["num_spaces"] < details[index-1]["num_spaces"] and len(parents) > 0:
+            parents.pop(-1)
+
+
+
+    def is_item_in_theme_entries(self, item_type, theme_path):
+        theme_entries = DB.fetch_theme_details()
+        #loop through results from database to see if theme details has already been recorded
+        for theme_entries_details in theme_entries:
+            print(theme_entries_details, "|", item_type, theme_path)
+            if theme_entries_details == (item_type, theme_path):
+                return True
+        return False
 
 
     def get_theme_ids(self) -> None:
-        #call database functions
-        db = DatabaseManagment()
 
-        #get all entries from item table to check if themes data have already been recorded
-        theme_entries = db.fetch_theme_details()
-        
         #create empty list which will be populated with dicts containing {M:[...], S:[...], path:"..."}
         theme_details_list = []
 
@@ -94,15 +135,7 @@ class Scraper():
             if r"     {more}" in themes:
                 themes.remove(r"     {more}")
 
-            #remove (num) of items in theme from theme name. Some themes have '(', ')' in their name e.g. '(other)'
-            #loop backwards of each theme:str and find the first '(' char. Split the string at that position
-            for theme_index, theme in enumerate(themes):
-                for char_index, char in enumerate(theme[::-1].strip("     ")):
-                    if char == "(":
-                        theme_name = theme[:len(theme)-char_index-2]
-                        #replace theme names with name without brackets
-                        themes[theme_index] = theme_name
-                        break
+            self.remove_brackets_from_theme_name(themes)
 
             #get all hyperlinks to all themes catalog page, displaying a list of all items 
             theme_links_table = self.driver.find_element(By.XPATH, '//*[@id="id-main-legacy-table"]/tbody/tr/td/table/tbody/tr[3]/td/table/tbody/tr/td/table/tbody/tr[2]')
@@ -112,16 +145,11 @@ class Scraper():
 
             #set default lists
             parents = [] ; details = []
-            print(len(themes), len(theme_links))
 
             #loop through each theme, create the path, click link then parse through all item ids.
             for index, theme in enumerate(themes):
 
-                #find first char in theme name to split.
-                for char in theme:
-                    if char != " ":
-                        first_letter = char
-                        break
+                first_letter = self.split_theme_name_by_first_char(theme)
                 
                 #the number of spaces before each theme to determine if it is a child theme of a parent / sub theme.
                 num_spaces = len(theme.split(first_letter)[0])
@@ -130,58 +158,53 @@ class Scraper():
                 if num_spaces == 0:
                     parents = []
 
-                #store a list of dicts storing "theme" and "num_spaces" to compare current theme with previous theme(s)
-                details.append({"theme":theme.strip("     ") + "//", "num_spaces":num_spaces})
-
-                #if a new level of indentaion is reached add the current theme as a new sub-parent theme
-                if details[index]["num_spaces"] > details[index-1]["num_spaces"]:
-                    parents.append(details[index-1]["theme"])
-
-                #if the previous themes spaces is < than current themes spaces the remove last element from parents as it is not a parent to any sub themes
-                elif details[index]["num_spaces"] < details[index-1]["num_spaces"] and len(parents) > 0:
-                    parents.pop(-1)
+                self.update_details_dict(details, num_spaces, index, parents, theme)
 
                 #complete the path "parent//sub/sub-sub"
                 theme_path = "".join(parents) + theme.strip("     ")
+
+                target_theme = "Star Wars"
+                if theme_path[0:len(target_theme)] != target_theme:
+                    continue
 
                 #some theme_paths have ' in theme causing errors in SQL statement
                 if "'" in theme_path:
                     theme_path = theme_path.replace("'", "")
 
-                #loop through results from database to see if theme details has already been recorded
-                entry_in = False
-                for theme_entries_details in theme_entries:
-                    if theme_entries_details == (item_type, theme_path):
-                        entry_in = True
-                        break
+                if " " in theme_path:
+                    theme_path = theme_path.replace(" ", "_")
 
-                #if entry is not recored then record the data (get item ids)
-                if not entry_in:
-                    #SCRAPE ITEM IDS, adds all item ids to dict for the current item type. Also resets for each theme
-                    theme_details = self.get_item_ids(theme_links[index], item_type)
+                #SCRAPE ITEM IDS, adds all item ids to dict for the current item type. Also resets for each theme
+                theme_details = self.get_item_ids(theme_links[index], item_type)
 
-                    #add the theme path to dict containing S / M list
-                    theme_details.update({
-                            "path": theme_path,
-                        })
-                    print(theme_details)
+                for item in theme_details[item_type]:
+                    print("Item ID - ", item)
+                    if item not in DB.get_all_itemIDs():
+                        item_info = RESP.get_response_data(f"items/MINIFIG/{item}")
+                        print("INSERTING -", item_info)
+                        DB.insert_item_info(item_info)
 
-                    #ADD TO DATABASE
-                    db.add_theme_details(theme_details, item_type)
+                #add the theme path to dict containing S / M list
+                theme_details.update({
+                        "path": theme_path,
+                    })
+                print("theme_details - ", theme_details)
 
-                    #add path and id to the dict which already contains lists of sets and minifigs END OF LOOP
-                    theme_details_list.append(theme_details)
+                #ADD TO DATABASE
+                DB.add_theme_details(theme_details, item_type)
+
+                #add path and id to the dict which already contains lists of sets and minifigs END OF LOOP
+                theme_details_list.append(theme_details)
 
 class ImageScrape():
 
     def __init__(self) -> None:
         self.driver = webdriver.Chrome()
         self.url = "https://www.bricklink.com/catalogTree.asp?itemType=S"
-        self.DB = DatabaseManagment()
 
 
     def loop_through_images(self):
-        self.item_ids = self.DB.theme_item_ids()
+        self.item_ids = DB.theme_item_ids()
         directory = r"..\App\static\App\sets"
         for item in self.item_ids:
             #1 = type, #0 = item_id
@@ -202,11 +225,11 @@ def main():
     cd OneDrive/Documents/programming/python/api's/bl_api/
     python -m pipenv shell
     '''
-    scraper = ImageScrape()
-    scraper.loop_through_images()
-    # scrape = Scraper()
-    # scrape.setup()
-    # scrape.get_theme_ids()
+    # scraper = ImageScrape()
+    # scraper.loop_through_images()
+    scrape = Scraper()
+    scrape.setup()
+    scrape.get_theme_ids()
 
 if __name__ == "__main__":
     main()
