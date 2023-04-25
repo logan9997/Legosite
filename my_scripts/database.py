8e406891169205ae5f2af7ca98ccf26d83c267b9
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-04-25 13:54:37 +0100

    .

--- my_scripts/database.py
+++ my_scripts/database.py
@@ -5,7 +5,7 @@ import os
 class DatabaseManagment():
 
     def __init__(self) -> None:
-        DEVELOPMENT = False
+        DEVELOPMENT = True
 
         if not DEVELOPMENT:
             file_name = "./heroku_database_credentials.txt"
@@ -187,7 +187,6 @@ class DatabaseManagment():
             ORDER BY Change DESC, I.item_id
             {limit_sql}
         '''
-        print(sql)
         return self.SELECT(sql)
     
 
@@ -618,14 +617,14 @@ class DatabaseManagment():
 
 
     def get_item_graph_info(self,item_id, metric, **kwargs) -> list[str]:
-        if kwargs.get("user_id") != -1 and kwargs.get("view") != None:
+        if kwargs.get("user_id") != -1 and kwargs.get("view", "item") != "item":
             sql = f'''
                 SELECT {metric}, date
-                FROM "App_price" P, "App_{kwargs.get("view")}" View, "App_item" I
-                WHERE user_id = {kwargs.get("user_id")}
+                FROM "App_price" P, "App_{kwargs.get("view")}" _view, "App_item" I
+                WHERE _view.user_id = {kwargs.get("user_id", -1)}
                     AND I.item_id = '{item_id}'
                     AND P.item_id = I.item_id
-                    AND I.item_id = View.item_id
+                    AND I.item_id = _view.item_id
                 GROUP BY I.item_id, P.date
                 ORDER BY date ASC
             '''
@@ -638,7 +637,6 @@ class DatabaseManagment():
                 GROUP BY {metric}, I.item_id, P.date
                 ORDER BY date ASC
                 '''        
-
         return self.SELECT(sql)
     
 
@@ -750,7 +748,10 @@ class DatabaseManagment():
         FROM "App_item"
         WHERE item_id = '{item_id}'
         '''
-        return self.SELECT(sql, fetchone=True)
+        result = self.SELECT(sql, fetchone=True)[0]
+        if result == None:
+            return 0
+        return result
     
     def get_new_items(self) -> list[str]:
         #REMOVE WHERE CLAUSE, only items where y_released = 1900 is stored in "App_price"
