2bd87944fdb762a61956002f6f8731b3d6859e33
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-05-24 12:45:45 +0100

    session cookie age set to 24hrs

--- legosite/settings.py
+++ legosite/settings.py
@@ -136,3 +136,4 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_COOKIE_AGE = 60 * 60 * 24
