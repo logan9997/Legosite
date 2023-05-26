bb109dd8c3af6dff1352a11b39cc69d709019541
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-05-26 22:47:57 +0100

    DEBUG_PROPAGATE_EXCEPTIONS=True in settings.py to prevent http error 500

--- legosite/settings.py
+++ legosite/settings.py
@@ -28,6 +28,7 @@ SECRET_KEY = 'django-insecure-yw92!lqu(z&m@5cl##=eoa&!3vhb!)qz2f587%le)=!snne6b-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 PYTHONUNBUFFERED = ''
 
