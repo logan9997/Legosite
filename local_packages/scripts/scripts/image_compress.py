958e80a65cea4d0ddf62e7e69cfbf2f5fb3f27cb
logan9997 <80635281+logan9997@users.noreply.github.com>
2023-05-27 11:32:23 +0100

    adding sets and minfigure images to repo

--- local_packages/scripts/scripts/image_compress.py
+++ local_packages/scripts/scripts/image_compress.py
@@ -1,11 +1,21 @@
 from PIL import Image
 from PIL import UnidentifiedImageError
+from pathlib import Path
 import os
+
+def get_size():
+    root_directory = Path('.')
+    return sum(f.stat().st_size for f in root_directory.glob('**/*') if f.is_file())
 
 img_folder = input('Path: ')
 
+size_before = get_size()
 
 for file in os.listdir(img_folder):
+
+    if '.jpeg' in file or 'jpg' in file:
+        continue
+
     img_path = os.path.join(img_folder, file)
     print(f'Compressing - {file}')
     try:
@@ -14,3 +24,7 @@ for file in os.listdir(img_folder):
         img.save(img_path, optimize=True)
     except UnidentifiedImageError:
         print(f'UnidentifiedImageError - {file}')
+
+size_after = get_size()
+
+print(f'Size before - {size_before}\nSize after - {size_after}\nDifference - {size_before - size_after}')
